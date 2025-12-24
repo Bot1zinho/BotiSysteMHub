72e66d2f23cfb845f3c49199d0e0d5f1b49f30ae
Botizinho <botisystemhub40926@gmail.com>
2025-12-24 11:23:35 -0300

    Update loader.lua

--- core/loader.lua
+++ core/loader.lua
@@ -2,6 +2,8 @@
 local Loader = {}
 
 function Loader:Init()
+    print("Loader iniciado")
+
     local Config = loadstring(game:HttpGet(
         "https://raw.githubusercontent.com/Bot1zinho/BotiSysteMHub/main/core/config.lua"
     ))()
