65233e44b15403589d2b6d884aead12e94cc5fb0
Botizinho <botisystemhub40926@gmail.com>
2025-12-24 10:56:52 -0300

    Update config.lua

--- core/config.lua
+++ core/config.lua
@@ -1,1 +1,15 @@
+-- core/config.lua
+local Config = {}
 
+Config.ScriptName = "SEU HUB"
+Config.Version = "v0.1"
+
+Config.Colors = {
+    Background = Color3.fromRGB(15, 15, 15),
+    Sidebar = Color3.fromRGB(20, 20, 20),
+    Text = Color3.fromRGB(255, 255, 255),
+    Purple = Color3.fromRGB(140, 90, 255),
+    Green = Color3.fromRGB(80, 200, 120)
+}
+
+return Config
