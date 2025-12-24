9f5d6e735a25f236d6dc0d897fb1657435ba8ad6
Botizinho <botisystemhub40926@gmail.com>
2025-12-24 11:13:21 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
--- main.lua
-print("Iniciando script...")
+print("Main carregado")
 
-local Loader = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/Bot1zinho/BotiSysteMHub/main/core/loader.lua"
-))()
+local success, result = pcall(function()
+    return loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Bot1zinho/BotiSysteMHub/main/core/loader.lua"
+    ))()
+end)
 
-Loader:Init()
+if not success then
+    warn("Erro ao carregar loader:", result)
+    return
+end
+
+result:Init()
