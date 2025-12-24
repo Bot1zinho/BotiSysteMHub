fe6ce4069de7352d598d55e16f4c723747bf2c92
Botizinho <botisystemhub40926@gmail.com>
2025-12-24 10:56:16 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 print("Iniciando script...")
 
 local Loader = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/SEU_USUARIO/BloxFruitsSeries/main/core/loader.lua"
+    "https://raw.githubusercontent.com/Bot1zinho/BotiSysteMHub/main/core/loader.lua"
 ))()
 
 Loader:Init()
